a3c051dc482cb482d105a5f3d70e8b251ecd26da
Malek Mneri <mmneri@anypli.com>
2023-03-29 13:51:38 +0100

    specify folder as an argument

--- spotify.py
+++ spotify.py
@@ -3,22 +3,39 @@ import os
 import spotipy
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
+from mutagen.mp3 import MP3
+from colorama import Fore
+import sys
 
-load_dotenv()
+args = sys.argv[1:]
+if len(args) == 0:
+    print('please specify folder')
+    sys.exit(1)
+folder = sys.argv[1:][0]
+if not os.path.isdir(folder):
+    print('no such directory: ' + folder)
 
-client_id = os.environ.get('SPOTIFY_CLIENT_ID')
-client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
-print(os.getcwd())
+print('Running Script on folder: ' + folder)
+for file in os.listdir(folder):
+    if file.endswith('.mp3'):
+        audio = MP3(folder+'/'+file)
+        title = audio.get("TIT2").text[0]
+        artist = audio.get("TPE1").text[0]
+        print('adding ' + Fore.RED+file + Fore.RESET+' as: \'' + Fore.CYAN+title + ' ' + artist + Fore.RESET+'\'')
 
-client_creds_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_creds_manager)
+#load_dotenv()
+#client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+#client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
+#
+#client_creds_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+#sp = spotipy.Spotify(client_credentials_manager=client_creds_manager)
+
+
+
+
+
+
 
 #song_name = 'Wow. Post malone'
 #results = sp.search(q=song_name, type='track', limit=1)
 #print(results)
-dir_list = os.listdir()
-
-print("Files and directories in current folder :")
-
-# prints all files
-print(dir_list)
