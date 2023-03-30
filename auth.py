84877bf6faced42d8b8261e060a2ee1c4663996b
Malek Mneri <mmneri@anypli.com>
2023-03-30 09:52:58 +0100

    write trackIds to txt file

--- auth.py
+++ auth.py
@@ -5,14 +5,15 @@ import sys
 import requests
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
 load_dotenv()
 client_id = os.environ.get('SPOTIFY_CLIENT_ID')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
+redirect_url = os.environ.get('SPOTIFY_REDIRECT_URI')
 
 
-def getToken():
+def get_token():
     auth_header = base64.b64encode((client_id + ':' + client_secret).encode('ascii')).decode('ascii')
     auth_options = {
         'url': 'https://accounts.spotify.com/api/token',
@@ -28,7 +29,7 @@ def getToken():
     return {"Authorization": "Bearer " + token}
 
 
-def checkArgs():
+def check_args():
     args = sys.argv[1:]
     if len(args) == 0:
         print('please specify folder')
@@ -41,5 +42,7 @@ def checkArgs():
 
 
 def auth():
+
     auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    #auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_url)
     return spotipy.Spotify(auth_manager=auth_manager)
--- spotify.py
+++ spotify.py
@@ -1,44 +1,32 @@
 import os
-from auth import getToken, checkArgs, auth
-from mutagen.mp3 import MP3
+from auth import get_token, check_args, auth
 from colorama import Fore
 from dotenv import load_dotenv
+from helpers import get_search
 
 load_dotenv()
 playlist_id = os.environ.get('PLAYLIST_ID')
 
-folder = checkArgs()
+folder = check_args()
 sp = auth()
-
-# token = getToken()
 
 print('Running Script in folder: ' + folder)
 trackIds = []
 batchIndex = 0
-offsetIndex = 10
+count = 0
 for file in os.listdir(folder):
-    if offsetIndex <= 20:
-        offsetIndex += 1
-        continue
-
     if file.endswith('.mp3'):
-        audio = MP3(folder + '/' + file)
-        title = audio.get("TIT2").text[0]
-        artist = audio.get("TPE1").text[0]
-        search = title + ' ' + artist
-        print('adding ' + Fore.CYAN + file + Fore.RESET + ' as: \'' + Fore.CYAN + search + Fore.RESET + '\'')
+        search = get_search(folder, file)
         results = sp.search(q=search, type='track', limit=1)
+
         if len(results['tracks']['items']) == 0:
             print('track not found!, ' + Fore.RED + search + Fore.RESET)
             continue
-        else:
-            print(results['tracks']['items'][0]['name'])
-        #trackIds.append(results['tracks']['items'][0]['uri'])
-        #sp.playlist_add_items(playlist_id, results['tracks']['items'][0]['uri'])
-        break
-        # if len(trackIds) >= 100:
-        #     sp.playlist_add_items(playlist_id, trackIds)
-        #     trackIds.clear()
-        #     batchIndex += 1
-        #     print(f"batch: {batchIndex} added!")
-# sp.playlist_add_items(playlist_id, trackIds)
+        with open(f"batch_{batchIndex}.txt", 'a') as f:
+            f.write(results['tracks']['items'][0]['uri'] + ',')
+            count += 1
+
+        if count >= 90:
+            count = 0
+            batchIndex += 1
+            print(f"batch: {batchIndex} added!")
